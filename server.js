4ef76d1c2758ab35952c6d6bc456979e17ea219d
Newton Migosi <newtonmigosi@gmail.com>
2020-10-08 14:38:09 +0300

    Completed first exercise

--- server.js
+++ server.js
@@ -2,4 +2,19 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+const express = require('express');
+
+const app = express();
+
+const jsonData = {count: 12, message: 'hey'};
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/data' ,(req, res) => {
+    res.json(jsonData);
+});
+
+app.listen(3000, () => console.log("server started at http://localhost:3000"));
