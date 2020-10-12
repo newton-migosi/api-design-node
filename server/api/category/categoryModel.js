04cee1aa62530350f5f77a813167bc20dcea9a30
Newton Migosi <newtonmigosi@gmail.com>
2020-10-12 15:32:54 +0300

    implemented models

--- server/api/category/categoryModel.js
+++ server/api/category/categoryModel.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-
+    name: {
+        type: String,
+        unique: true,
+    },
 });
 
 module.exports = mongoose.model('category', CategorySchema);
--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -2,7 +2,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
+    text: {
+        type: String,
+    },
+
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+
+    categories: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: 'category',
+        }],
+    }
 });
 
 module.exports = mongoose.model('post', PostSchema);
